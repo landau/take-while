01a0215eabac984c0b0a4c5370e68c452e02ddce
Trevor Landau <landau@users.noreply.github.com>
2020-05-12 09:47:54 -0400

    test: fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  extends: ['@landau/eslint-config', '@landau/eslint-config/rules/jsdoc'],
+  extends: ["@landau/eslint-config", "@landau/eslint-config/rules/jsdoc"],
   rules: {
-    'prefer-destructuring': 'off',
-    'prefer-rest-params': 'off',
-    'prefer-spread': 'off',
+    "prefer-destructuring": "off",
+    "prefer-rest-params": "off",
+    "prefer-spread": "off",
   },
 };
